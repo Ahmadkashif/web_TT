b363d605554f5aab3481ccf669a0bb44a2789a25
Ahmadkashif <ahmadkashif983@gmail.com>
2020-09-11 21:24:05 +0500

    "9/11/2020"

---
+++ src/TT/ContactUs/ContactUs.js
@@ -0,0 +1,12 @@
+import React , {Component} from 'react'
+import JumboCnt from './jumboCntUS/jumboCnt'
+class ContactUs extends Component{
+    render(){
+        return(
+            <div>
+                <JumboCnt/>
+            </div>
+        )
+    }
+}
+export default ContactUs
---
+++ src/TT/ContactUs/jumboCntUS/jumboCnt.js
@@ -0,0 +1,16 @@
+import React ,{Component} from 'react'
+
+class JumboCnt extends Component{
+    render(){
+        return(
+            <div>
+                 <div className="jumbotron jumbotron-fluid jumbo1AboutUs ">
+                    <div className="container d-flex justify-content-center">
+                        <h1 className="display-4 heading_abtus">Contact Us</h1>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+export default JumboCnt;
