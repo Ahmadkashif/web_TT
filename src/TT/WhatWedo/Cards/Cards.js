20176d7da70b40790182a72af557623e1b15073b
Ahmadkashif <ahmadkashif983@gmail.com>
2020-09-11 17:22:23 +0500

    9/11/2020

--- src/TT/WhatWedo/Cards/Cards.js
+++ src/TT/WhatWedo/Cards/Cards.js
@@ -3,18 +3,18 @@ import './cards.css'
 class Card extends Component{
     render(){
         return(
-            <div>
-                <div className="card rounded-2">
+                <div className="card rounded-lg cardCust ">
                     <div className="card-body ">
                     <img class="ui medium circular image" src={this.props.icon}/>
-                        <h5 className="card-title">Card title</h5>
+                        <h5 className="card-title">{this.props.label}</h5>
                         <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="card-link">Card link</a>
-                        <a href="#" className="card-link">Another link</a>
+                        <div className = "">
+                        <a href="#" className="card-link">Learn more{' >'}</a>
+                        </div>
                     </div>
                 </div>
-            </div>
+            
         );
     }
 }
--- src/TT/WhatWedo/whatWedo.js
+++ src/TT/WhatWedo/whatWedo.js
@@ -1,6 +1,12 @@
 import React ,{Component} from 'react'
 import Card from './Cards/Cards'
+import './WWEdo.css'
 import GamingIcon from '../../images/icons/gaming.png'
+import AppDevIcon from '../../images/icons/appdev.png'
+import WebDevIcon from '../../images/icons/webdev.png'
+import UIUXIcon from '../../images/icons/uiux.png'
+import DigiIcon from '../../images/icons/digitalmarketing.png'
+
 class WWedo extends Component{
 
     state= {
@@ -11,22 +17,35 @@ class WWedo extends Component{
                 "Digital Marketing"
                 ]
     };
+
     render(){
         return(
             <div>
-                <div className = "container">
-                <div className="jumbotron jumbotron-fluid" id = "WWeDoJumbo">
+                
+                <div className="jumbotron jumbotron-fluid vertSpacing col-xl-12 col-lg-12 col-md-12 col-sm-12" id = "WWeDoJumbo">
                     <h1 className = "display-4">What We Do</h1>
+                    <div className = "container">
 
-                    <div className = "row">
-                        <div className = "col">
+                    
+                    <div className = "row vertSpacing d-flex justify-content-around">
+                        <div className = "col-xl-4 col-lg-4 col-md-4 col-sm-12">
                        <Card icon = {GamingIcon} label = {this.state.cardNames[0]}/>
                         </div>
-                        <div className = "col"></div>
-                        <div className = "col"></div>
+                        <div className = "col-xl-4 col-lg-4 col-md-4 col-sm-12">
+                        <Card icon = {AppDevIcon} label = {this.state.cardNames[1]}/>
+                        </div>
+                        <div className = "col-xl-4 col-lg-4 col-md-4 col-sm-12">
+                        <Card icon = {WebDevIcon} label = {this.state.cardNames[2]}/>
+                        </div>
                     </div>
-                    <div className = "row">
-                        
+
+                    <div className = "row d-flex justify-content-around  ">
+                    <div className = "col-xl-4 col-lg-4 col-md-4 col-sm-12">
+                       <Card icon = {UIUXIcon} label = {this.state.cardNames[3]}/>
+                        </div>
+                        <div className = "col-xl-4 col-lg-4 col-md-4 col-sm-12">
+                        <Card icon = {DigiIcon} label = {this.state.cardNames[4]}/>
+                        </div>
                     </div>
                 </div>
                 </div>
