d06d4432bc7b5e1d4612990a48b3638536543c52
Ahmadkashif <ahmadkashif983@gmail.com>
2020-09-14 20:01:57 +0500

    9/14/2020

--- src/TT/Portfolio/Portfolio_MainPage/PT_jumbo1/PT_jumbo1.js
+++ src/TT/Portfolio/Portfolio_MainPage/PT_jumbo1/PT_jumbo1.js
@@ -3,8 +3,12 @@ import './PT_jumbo1.css'
 class PT_jumbo1  extends Component{
     render(){
         return(
-            <div className = "PTJM1_main">
-
+            <div className = "">
+                <div className="jumbotron jumbotron-fluid PTJM1_main">
+                    <div className="container d-flex justify-content-center mt-5">
+                        <h1 className="display-4">Portfolio</h1>
+                    </div>
+                </div>  
             </div>
         )
     }
--- src/TT/Portfolio/Portfolio_MainPage/PortMainPage.js
+++ src/TT/Portfolio/Portfolio_MainPage/PortMainPage.js
@@ -1,9 +1,10 @@
 import React , {Component} from 'react';
+import Jumbo1 from './PT_jumbo1/PT_jumbo1'
 class PortMainPage extends Component{
     render(){
         return (
             <div>
-                
+                <Jumbo1/>
             </div>
         )
     }
