9aa4f2b1898361c26c979ec6252c40ac1fc3580e
Ahmadkashif <ahmadkashif983@gmail.com>
2020-09-14 18:18:36 +0500

    9/14/2020

--- src/TT/ContactUs/CU_form/CU_form.js
+++ src/TT/ContactUs/CU_form/CU_form.js
@@ -10,7 +10,6 @@ class CU_form extends Component{
                     <input type="email" className="form-control inputCUForm" id="exampleInputEmail1" aria-describedby="emailHelp"/>
                     <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                 </div>
-
                 <div className="form-group">
                     <label for="exampleFormControlTextarea1">Example textarea</label>
                     <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
