3b66b66be00b64fa25045665107b1f8aa138f8ac
Ahmadkashif <ahmadkashif983@gmail.com>
2020-09-14 19:37:57 +0500

    9/14/2020

---
+++ src/TT/Portfolio/Portfolio_MainPage/PT_jumbo1/PT_jumbo1.js
@@ -0,0 +1,12 @@
+import React , {Component } from 'react'
+
+class PT_jumbo1  extends Component{
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+export default PT_jumbo1;
