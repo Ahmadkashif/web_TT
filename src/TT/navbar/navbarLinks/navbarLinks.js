a0be2561da6fc387dad02506cbe2ee15a622b262
Ahmadkashif <ahmadkashif983@gmail.com>
2020-09-14 19:36:35 +0500

    9/14/2020

--- src/TT/navbar/navbarLinks/navbarLinks.js
+++ src/TT/navbar/navbarLinks/navbarLinks.js
@@ -5,12 +5,12 @@ class NavbarLinks extends Component{
     render(){
         return (
             <div className = "navCustDesign d-flex justify-content-center">
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <button className="navbar-toggler d-flex ml-auto" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNavDropdown">
                         <ul className="navbar-nav">
-                        <li className="nav-item active">
+                        <li className="nav-item active d-flex justify-content-center">
                             <NavLink className="nav-link navCustDesign_spacingX" to="/" exact activeClassName = "Nav_link_active_cust">Home <span className="sr-only">(current)</span></NavLink>
                         </li>
                         <li className="nav-item">
