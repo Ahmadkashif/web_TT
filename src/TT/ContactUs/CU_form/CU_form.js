0d08e51689473a97072fc6f8416364e6763d9353
Ahmadkashif <ahmadkashif983@gmail.com>
2020-09-14 18:29:08 +0500

    9/14/2020

--- src/TT/ContactUs/CU_form/CU_form.js
+++ src/TT/ContactUs/CU_form/CU_form.js
@@ -6,18 +6,15 @@ class CU_form extends Component{
         <div className = "CU_form">
             <form>
                 <div className="form-group ">
-                    <label for="exampleInputEmail1">Email address</label>
+                    <label for="exampleInputEmail1" className = "CU_form_labels">Email address</label>
                     <input type="email" className="form-control inputCUForm" id="exampleInputEmail1" aria-describedby="emailHelp"/>
                     <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                 </div>
                 <div className="form-group">
-                    <label for="exampleFormControlTextarea1">Example textarea</label>
+                    <label for="exampleFormControlTextarea1"  className = "CU_form_labels">Example textarea</label>
                     <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                 </div>
-                <div class="form-group form-check">
-                    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-                    <label className="form-check-label" for="exampleCheck1">Check me out</label>
-                </div>
+               
                 <button type="submit" className="btn btn-primary">Submit</button>
             </form>
         </div>
