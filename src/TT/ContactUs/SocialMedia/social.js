380f1cd9bc2b547226e6c245142b879d1a30f2a2
Ahmadkashif <ahmadkashif983@gmail.com>
2020-09-14 21:10:55 +0500

    9/14/2020'

--- src/TT/ContactUs/SocialMedia/social.js
+++ src/TT/ContactUs/SocialMedia/social.js
@@ -3,8 +3,8 @@ import React , {Component} from 'react'
 class Social extends Component{
     render(){
         return (
-            <div>
-                
+            <div >
+
             </div>
         )
     }
