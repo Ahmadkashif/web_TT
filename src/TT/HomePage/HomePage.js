d8a39886132e3e4996c29570ba6b3b4fc9015046
Ahmadkashif <ahmadkashif983@gmail.com>
2020-09-15 20:00:07 +0500

    9/15/2020

--- src/TT/HomePage/HomePage.js
+++ src/TT/HomePage/HomePage.js
@@ -2,6 +2,7 @@ import React ,{ Component} from 'react'
 import Jumbo1 from './jumbo1_homepage/jumbo1'
 import OverView from './OverView/overview'
 import WhatWEdo from './WhatWedo/whatWedo'
+import OurWork from './OurWork/OurWork'
 class HomePage extends Component{
 
     render(){
@@ -11,14 +12,7 @@ class HomePage extends Component{
             <OverView/>
             <WhatWEdo/>
             <div className= "jumbotron mainPage_vert_space">
-            <div className="container OurWorkShowing">
-                <h1 className="display-4 d-flex justify-content-center">Our Work</h1>
-                <p className="lead  d-flex justify-content-center">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-                <p  className="d-flex justify-content-center">It uses utility classes for typography and spacing to space content out within the larger container.</p>
-                <div className= "d-flex justify-content-center mainPage_vert_space">
-                <a className="btn btn-primary btn-lg" href="/portfolio" role="button">Learn more</a>
-                </div>                
-            </div>
+           <OurWork/>
             </div>
         </div>
         )
