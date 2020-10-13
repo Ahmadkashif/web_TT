212d12f67136e24881e7c240b2273063c7e66597
Ahmadkashif <ahmadkashif983@gmail.com>
2020-10-13 15:24:33 +0500

    all done

--- src/TT/Portfolio/Apps/Apps.js
+++ src/TT/Portfolio/Apps/Apps.js
@@ -6,12 +6,12 @@ class Apps extends Component{
             <div className = "container mb-5">
                 <div className = "row  pl-1 pr-1 m-1">
                 <div className = "col-xl-4 col-lg-4 col-md-6 col-sm-12 d-flex justify-content-center pt-2">
-                    <a className = "portfolioViewProductLink" href = "#">
+                    <a className = "portfolioViewProductLink" disabled>
                         <div className = "AppGame">
                         <div className = "App1">
                             <div className = "overlay blackOverlay d-flex flex-column justify-content-center">
                                 <h1 className = " display-4 overlayText ProductName">
-                                    Product Name
+                                    Shophood
                                 </h1>
                                 <h1 className = "display-4 overlayText">
                                     <strong>View Product</strong>
@@ -22,12 +22,12 @@ class Apps extends Component{
                    </a>
                 </div>
                 <div className = "col-xl-4 col-lg-4 col-md-6 col-sm-12 d-flex justify-content-center pt-2">
-                    <a className = "portfolioViewProductLink" href = "#">
+                    <a className = "portfolioViewProductLink"  disabled>
                         <div className = "AppGame">
                         <div className = "App2">
                             <div className = "overlay blackOverlay d-flex flex-column justify-content-center">
                                 <h1 className = " display-4 overlayText ProductName">
-                                    Product Name
+                                    Seeksi
                                 </h1>
                                 <h1 className = "display-4 overlayText">
                                     <strong>View Product</strong>
@@ -38,12 +38,12 @@ class Apps extends Component{
                    </a>
                 </div>
                 <div className = "col-xl-4 col-lg-4 col-md-6 col-sm-12 d-flex justify-content-center pt-2">
-                    <a className = "portfolioViewProductLink" href = "#">
+                    <a className = "portfolioViewProductLink" >
                         <div className = "AppGame">
                         <div className = "App3">
                             <div className = "overlay blackOverlay d-flex flex-column justify-content-center">
                                 <h1 className = " display-4 overlayText ProductName">
-                                    Product Name
+                                    StreamIT
                                 </h1>
                                 <h1 className = "display-4 overlayText">
                                     <strong>View Product</strong>
